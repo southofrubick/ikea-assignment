f100cb254b7929c56109fbf999eb65b4f040b482
MartinWJonsson <martin.jonsson@octanelending.com>
2025-08-24 13:26:23 +0200

    add get all products API

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
+	"net/http"
 
 	"github.com/labstack/echo/v4"
 	"github.com/southofrubick/ikea-assignment/db"
+	"github.com/southofrubick/ikea-assignment/entity"
 )
 
 
@@ -17,6 +20,23 @@ func main() {
 	defer pool.Close()
 
 	e := echo.New()
+
+	e.GET("/api/products", func(c echo.Context) error {
+		var products []entity.Product
+
+		products, err = db.GetAllProducts(pool)
+		if err != nil {
+			return c.String(http.StatusInternalServerError, "Failed to fetch products")
+		}
+		log.Printf("Fetched %d products from the database", len(products))
+
+		productsJson, err := json.Marshal(products)
+		if err != nil {
+			return c.String(http.StatusInternalServerError, "Failed to marshal products to JSON")
+		}
+
+		return c.String(http.StatusOK, string(productsJson))
+	})
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
