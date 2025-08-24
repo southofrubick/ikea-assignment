80d89e55f52189216e63e78c1575ce4d65ad8206
MartinWJonsson <martin.jonsson@octanelending.com>
2025-08-24 13:26:39 +0200

    add get product detail API

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 	"github.com/southofrubick/ikea-assignment/db"
@@ -36,6 +37,28 @@ func main() {
 		}
 
 		return c.String(http.StatusOK, string(productsJson))
+	})
+
+	e.GET("/api/products/:id", func(c echo.Context) error {
+		var product *entity.Product
+
+		id, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			return c.String(http.StatusBadRequest, "Invalid product ID")
+		}
+
+		product, err = db.GetProductByID(pool, id)
+		if err != nil {
+			return c.String(http.StatusInternalServerError, "Failed to fetch product")
+		}
+		log.Printf("Fetched %d product from the database", product)
+
+		productJson, err := json.Marshal(product)
+		if err != nil {
+			return c.String(http.StatusInternalServerError, "Failed to marshal product to JSON")
+		}
+
+		return c.String(http.StatusOK, string(productJson))
 	})
 
 	e.Logger.Fatal(e.Start(":8080"))
